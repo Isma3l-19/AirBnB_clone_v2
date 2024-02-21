d6748b914ccbda68798e81da0a4f892998cb9553
LINNETWAHOME02 <98949954+LINNETWAHOME02@users.noreply.github.com>
2024-02-21 17:47:55 +0300

    Update do_create() in console.py

--- console.py
+++ console.py
@@ -113,18 +113,37 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    """ Console improvements - Updated on do_create() """
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+    """ Create an object of any class"""
+    args = args.split()
+    if not args:
+        print("** class name missing **")
+        return
+    elif args[0] not in HBNBCommand.classes:
+        print("** class doesn't exist **")
+        return
+    new_instance = HBNBCommand.classes[args[0]]()
+    for arg in args[1:]:
+        try:
+            key, value = arg.split('=')
+            # Replace underscore with space for string type
+            if value[0] == '"' and value[-1] == '"':
+                value = value.replace('_', ' ')
+            # Try converting to float and int
+            else:
+                try:
+                    value = float(value)
+                    if value.is_integer():
+                        value = int(value)
+                except ValueError:
+                    continue
+            setattr(new_instance, key, value)
+        except ValueError:
+            continue
+    storage.save()
+    print(new_instance.id)
+    storage.save()
 
     def help_create(self):
         """ Help information for the create method """
