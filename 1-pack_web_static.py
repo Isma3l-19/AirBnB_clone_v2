950af9f64baeb8a41ca1820efba91498afddb080
Isma3l-19 <ismaelnyambu.99@gmail.com>
2024-03-06 03:54:21 -0800

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# fabric module
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """script that generates a .tgz archive from the contents
+    of the web_static folder of this repository
+    """
+
+    d = datetime.now()
+    now = d.strftime('%Y%m%d%H%M%S')
+
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
