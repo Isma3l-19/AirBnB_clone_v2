663846aa010cfabf759f0d6d3d7f78aee7b8e42f
Isma3l-19 <133232846+Isma3l-19@users.noreply.github.com>
2024-03-23 17:50:45 +0000

    chnages 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 """
-creating three routes using flask
+script using Flask to create a web application with three routes
 """
 from flask import Flask, escape
 
+
 app = Flask(__name__)
 
-#displaying 'Hello HBNB!'
+
+#Route to display "Hello HBNB!"
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
-#displaying HBNB
+
+#Route to display "HBNB"
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
+#Route to display "C " followed by the value of the text variable
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    text = text.replace('_', ' ')
-    return f'C {escape(text)}'
+def display_text(text):
+    processed_text = escape(text).replace('_', ' ')
+    return f'C {processed_text}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
